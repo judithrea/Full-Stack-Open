d0e7968b04a7e599fb40351efe8b79334eaf7478
Judith Rea Nogales <judithrea98@gmail.com>
2025-09-15 00:39:09 +0200

    part 1 done

---
+++ part 1/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (course) => {
+  return (
+    <div>
+      <h1>{course.course}</h1>
+    </div>
+  )
+}
+
+const Part = ({part, exercise}) => {
+  return (
+    <div>
+      <p>{part} {exercise}</p>
+    </div>
+  )
+}
+
+const Content = ({parts, exercises}) => {
+    return (
+      <div>
+        <Part part={parts.part1} exercise={exercises.exercises1}/>
+        <Part part={parts.part2} exercise={exercises.exercises2}/>
+        <Part part={parts.part3} exercise={exercises.exercises3}/>
+      </div>
+    )
+  }
+
+const Total = ({exercises}) => {
+  return (
+    <p>Number of exercises {exercises.exercises1 + exercises.exercises2 + exercises.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = {
+    part1: 'Fundamentals of React',
+    part2: 'Using props to pass data',
+    part3: 'State of a component'
+  };
+  const exercises = {
+    exercises1: 10,
+    exercises2: 7,
+    exercises3: 14
+  };
+  
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts} exercises={exercises}/>
+      <Total exercises={exercises}/>
+    </div>
+  )
+}
+
+export default App
