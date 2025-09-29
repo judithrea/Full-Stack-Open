724c7e667cac4075331025eb505e0a419846aaf4
Judith Rea Nogales <judithrea98@gmail.com>
2025-09-30 00:33:40 +0200

    returned courseinfo to earlier commit for part 1

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -1,55 +1,59 @@
-import Courses from './components/Courses'
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Part = ({part, exercise}) => {
+  return (
+    <div>
+      <p>{part} {exercise}</p>
+    </div>
+  )
+}
+
+const Content = ({course}) => {
+    return (
+      <div>
+        <Part part={course.parts[0].name} exercise={course.parts[0].exercises}/>
+        <Part part={course.parts[1].name} exercise={course.parts[1].exercises}/>
+        <Part part={course.parts[2].name} exercise={course.parts[2].exercises}/>
+      </div>
+    )
+  }
+
+const Total = ({course}) => {
+  return (
+    <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+  )
+}
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      <Courses courses={courses} />
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
