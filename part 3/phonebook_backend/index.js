5ae037bf17be96d309d6d309f668d10d9aa995a5
Judith Rea Nogales <judithrea98@gmail.com>
2025-10-21 14:04:15 +0200

    Added proxy to front and rm cors from back (3b done)

--- part 3/phonebook_backend/index.js
+++ part 3/phonebook_backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+//const cors = require('cors')
 
 const app = express()
 
@@ -35,7 +35,7 @@ morgan.token('body', (req) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
+//app.use(cors())
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
