a1c14b5a2aaf6e7ca0e77437497be4caaccf7807
Judith Rea Nogales <judithrea98@gmail.com>
2025-11-12 16:36:22 +0100

    Toggle blog details (5.7)

--- part 5/bloglist-frontend/src/components/Blog.jsx
+++ part 5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleViewBtn = {display: detailsVisible ? 'none' : ''}
+  const toggleDetails = {display: detailsVisible ? '' : 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button style={toggleViewBtn} onClick={() => setDetailsVisible(true)}>View</button>
+        <button style={toggleDetails} onClick={() => setDetailsVisible(false)}>hide</button>
+      </div>
+      <div style={toggleDetails} >
+        {blog.url} {<br/>}
+        {blog.likes}  <button>like</button> {<br/>}
+        {blog.user ? blog.user.name : ''}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
