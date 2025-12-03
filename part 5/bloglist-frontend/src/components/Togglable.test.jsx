e136297e69ba5dd822d513804e5b89cb90cacf5c
Judith Rea Nogales <judithrea98@gmail.com>
2025-12-03 20:31:58 +0100

    Test clicking like 2x calls evtHandler2x

--- part 5/bloglist-frontend/src/components/Togglable.test.jsx
+++ part 5/bloglist-frontend/src/components/Togglable.test.jsx
@@ -2,13 +2,15 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Togglable from './Togglable'
 
+const mockHandler = vi.fn()
+
 describe('<Togglable />', () => {
   beforeEach(() => {
     render (
       <Togglable showButton="view">
         <div>
           'www.testurl.com' {<br/>}
-          0  <button>like</button> {<br/>}
+          0  <button onClick={mockHandler}>like</button> {<br/>}
         </div>
       </Togglable>
     )
@@ -16,25 +18,26 @@ describe('<Togglable />', () => {
   })
 
   test('url and likes show when view button is clicked', async () => {
-    // render (
-    //   <Togglable showButton="view">
-    //     <div>
-    //       'www.testurl.com' {<br/>}
-    //       0  <button>like</button> {<br/>}
-    //     </div>
-    //   </Togglable>
-    // )
-    // screen.debug()
-    const mockHandler = vi.fn()
     const user = userEvent.setup()
     const button = screen.getByText('view')
     await user.click(button)
-    // expect(button).toBeDefined()
-    // expect(mockHandler.mock.calls).toHaveLength(1)
-    
-    const el = screen.getByText('www.testurl.com', {exact:false})
-    screen.debug(el)
-    expect(el).toBeVisible()
+
+    const url = screen.getByText('www.testurl.com', {exact:false})
+    const likes = screen.getByText(0, {exact:false})
+
+    expect(url).toBeVisible()
+    expect(likes).toBeVisible()
+  })
+
+  test('clicking like twice calls event handler twice', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const likeBtn = screen.getByText('like')
+    await user.click(likeBtn)
+    await user.click(likeBtn)
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
 
